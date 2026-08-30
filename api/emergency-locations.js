75da0e4cb3bdc3025182df103e33d641ac701ad3
Saba Hassim Syed <sabahassimsyed@gmail.com>
2026-08-30 16:41:44 +0530

    fix: enable CORS for emergency locations API

--- api/emergency-locations.js
+++ api/emergency-locations.js
@@ -1,6 +1,14 @@
 import { prototypeEmergencyLocations } from "../backend/data/emergencyLocationsData.js";
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   res.status(200).json({
     source: "prototype_demo_data",
     count: prototypeEmergencyLocations.length,
